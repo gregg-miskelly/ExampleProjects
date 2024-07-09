0103bc6fef5411ef535bbbf4856a8353fa74301f
Gregg Miskelly <greggm@microsoft.com>
2024-07-08 17:05:22 -0700

    Add C# Hello World console app project

---
+++ src/CsHelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Welcome to C# Hello world, running on {0} ({1}).", RuntimeInformation.RuntimeIdentifier, RuntimeInformation.ProcessArchitecture);
+
+var numbers = new List<int>();
+while (true)
+{
+    Console.WriteLine("Enter a number or 'q' to quit");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+
+    if (int.TryParse(input, out var number))
+    {
+        numbers.Add(number);
+    }
+    else
+    {
+        Console.WriteLine("Invalid number");
+    }
+}
+
+Console.WriteLine("Numbers entered: {0}", string.Join(", ", numbers));
